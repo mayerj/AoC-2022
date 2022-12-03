72f1d715fb4d75598b9092d5070ad0879deef14f
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2022-12-03 10:48:48 -0500

    day 2 part 2

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,11 +1,12 @@
 use core::fmt;
 use std::fs;
 
-#[derive(Eq, PartialEq)]
+#[repr(i32)]
+#[derive(Eq, PartialEq, Clone, Copy)]
 enum Selection {
-    Rock,
-    Paper,
-    Scissors,
+    Rock = 0,
+    Paper = 1,
+    Scissors = 2,
 }
 
 impl fmt::Display for Selection {
@@ -14,6 +15,23 @@ impl fmt::Display for Selection {
             Selection::Rock => write!(f, "Rock"),
             Selection::Paper => write!(f, "Paper"),
             Selection::Scissors => write!(f, "Scissors"),
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Clone, Copy)]
+enum NeededResult {
+    Win,
+    Loss,
+    Draw,
+}
+
+impl fmt::Display for NeededResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            NeededResult::Win => write!(f, "Win"),
+            NeededResult::Loss => write!(f, "Loss"),
+            NeededResult::Draw => write!(f, "Draw"),
         }
     }
 }
@@ -27,11 +45,37 @@ fn to_selection(input: &str) -> Option<Selection> {
     };
 }
 
+fn to_result(input: &str) -> Option<NeededResult> {
+    return match input {
+        "X" | "A" => Some(NeededResult::Loss),
+        "Y" | "B" => Some(NeededResult::Draw),
+        "Z" | "C" => Some(NeededResult::Win),
+        _ => Option::None,
+    };
+}
+
 fn get_points(input: &Selection) -> i32 {
     return match input {
         Selection::Rock => 1,
         Selection::Paper => 2,
         Selection::Scissors => 3,
+    };
+}
+
+fn to_selection_from_i32(input: i32) -> Option<Selection> {
+    return match input {
+        0 => Some(Selection::Rock),
+        1 => Some(Selection::Paper),
+        2 => Some(Selection::Scissors),
+        _ => Option::None,
+    };
+}
+fn get_my_choice(opponent: &Selection, needed_result: &NeededResult) -> Selection {
+    let opponent_value = *opponent as i32;
+    return match needed_result {
+        NeededResult::Draw => opponent.clone(),
+        NeededResult::Win => to_selection_from_i32((3 + opponent_value + 1) % 3).unwrap(),
+        NeededResult::Loss => to_selection_from_i32((3 + opponent_value - 1) % 3).unwrap(),
     };
 }
 
@@ -68,7 +112,12 @@ fn get_win_points(my_selection: &Selection, opponent: &Selection) -> i32 {
 }
 
 fn main() {
-    let data = fs::read_to_string("data.txt").expect("unable to read files");
+    assert!(play("sample.txt") == 12);
+    println!("{}", play("data.txt"));
+}
+
+fn play(input: &str) -> i32 {
+    let data = fs::read_to_string(input).expect("unable to read files");
 
     let chunks = data.split("\r\n").collect::<Vec<&str>>();
 
@@ -78,7 +127,9 @@ fn main() {
         let choices = line.split(" ").collect::<Vec<_>>();
 
         let opponent_choice = to_selection(choices.get(0).unwrap()).unwrap();
-        let my_choice = to_selection(choices.get(1).unwrap()).unwrap();
+        let needed_result = to_result(choices.get(1).unwrap()).unwrap();
+
+        let my_choice = get_my_choice(&opponent_choice, &needed_result);
 
         let choice_points = get_points(&my_choice);
 
@@ -93,4 +144,6 @@ fn main() {
     }
 
     println!("{}", total);
+
+    return total;
 }
