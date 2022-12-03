93087d53c01d7b2ae3701bf55fc2fc359911f851
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2022-12-03 00:25:04 -0500

    day 1 part 2

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,21 +1,25 @@
 use std::fs;
 
+struct Elf {
+    index: i32,
+    calories: i32,
+}
+
 fn main() {
     let data = fs::read_to_string("src/data.txt").expect("unable to read files");
 
     let chunks = data.split("\r\n").collect::<Vec<&str>>();
 
-    let mut max: i32 = 0;
-    let mut max_index: i32 = 0;
+    let mut elves: Vec<Elf> = Vec::new();
     let mut current: i32 = 0;
     let mut current_index: i32 = 0;
 
     for line in chunks {
         if line.trim().len() == 0 {
-            if current > max {
-                max = current;
-                max_index = current_index;
-            }
+            elves.push(Elf {
+                index: current_index,
+                calories: current,
+            });
             current_index += 1;
             current = 0;
             continue;
@@ -24,5 +28,15 @@ fn main() {
         current += line.parse::<i32>().expect("not a number?");
     }
 
-    print!("{} - {} out of {}", max_index, max, current_index)
+    elves.sort_by(|x, y| y.calories.cmp(&x.calories));
+
+    let mut sum: i32 = 0;
+    for n in 0..3 {
+        let elf = elves.get(n).expect("no elf");
+        sum += elf.calories;
+
+        println!("{} - {} out of {}", elf.index, elf.calories, elves.len())
+    }
+
+    println!("{}", sum);
 }
