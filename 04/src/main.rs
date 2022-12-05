f8669d899313d429065465dcda6b7c0d82232148
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2022-12-04 20:40:28 -0500

    day 4 part 2

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -1,11 +1,13 @@
 use std::fs;
 
 fn main() {
-    assert!(play("sample.txt") == 2);
-    println!("{}", play("data.txt"));
+    assert!(play("sample.txt", &is_subset) == 2);
+    println!("{}", play("data.txt", &is_subset));
+    assert!(play("sample.txt", &is_overlap) == 4);
+    println!("{}", play("data.txt", &is_overlap));
 }
 
-fn play(input: &str) -> usize {
+fn play(input: &str, predicate: &dyn Fn((i32, i32), (i32, i32)) -> bool) -> usize {
     let data = fs::read_to_string(input).expect("unable to read files");
 
     let chunks = data.split("\r\n").collect::<Vec<&str>>();
@@ -15,7 +17,7 @@ fn play(input: &str) -> usize {
     let ranges = pairs.map(|(first, second)| (to_range(first), to_range(second)));
 
     return ranges
-        .filter(|x| is_subset(x.0, x.1) || is_subset(x.1, x.0))
+        .filter(|x| predicate(x.0, x.1) || predicate(x.1, x.0))
         .count();
 }
 
@@ -28,4 +30,10 @@ fn to_range(input: &str) -> (i32, i32) {
 
 fn is_subset(first: (i32, i32), second: (i32, i32)) -> bool {
     return first.0 <= second.0 && first.1 >= second.1;
+}
+
+fn is_overlap(first: (i32, i32), second: (i32, i32)) -> bool {
+    let range = first.0..=first.1;
+
+    return range.contains(&second.0) || range.contains(&second.1);
 }
